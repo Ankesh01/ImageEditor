09d818753f8c9f3218277e54e017b17d9133ca6b
Ankesh01 <ankeshagrawal440@gmail.com>
2024-07-04 05:59:13 +0530

    Tried to change logo

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
-const local_api = "http://localhost:5000";
-const production_api = "";
+// const local_api = "http://localhost:5000";
+const production_api = "https://image-editor-red-two.vercel.app";
 
 const token = localStorage.getItem("canva_token");
 
 const api = axios.create({
-  baseURL: local_api,
+  baseURL: production_api,
   headers: {
     Authorization: token ? `Bearer ${token}` : "",
   },
